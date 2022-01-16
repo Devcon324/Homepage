9fc4d38f321ad111d0314eb72ef01b438cdafb7d
Devcon324 <devknight324@gmail.com>
2022-01-16 12:19:45 -0500

    Attempt at making auto sldieshow in JavaScript

--- JavaScript/slide.js
+++ JavaScript/slide.js
@@ -3,6 +3,19 @@ slides[0] = "Pictures/PLsquat.jpg";
 slides[1] = "Pictures/PLbench.jpg";
 slides[2] = "Pictures/PLdeadlift.jpg";
 let i = 0;
+
+function autoslide(){
+	for (i = 0; i = slides.length; i++){
+		if (i = slides.length){
+			i = 0;
+		}
+		document.getElementById('slide').src = slides[i];
+		setTimeout(autoslide, 2000);
+	}
+}
+
+
+
 function next(){
 	i = i + 1;
 	if (i > 2){
